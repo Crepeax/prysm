7122c3744d82fd6c9f5ac8c70a6e9d80b45538af
Jan Dickmann <jan@dickmann.tv>
2020-05-17 19:03:27 +0200

    Created unmute command

---
+++ commands/unmute.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+const client = require('../index').client;
+const fs = require('fs');
+const config = require('../config.json');
+
+module.exports = {
+    name: 'unmute',
+    description: '',
+    syntax: '',
+    guildOnly: true,
+    perms: ['SEND_MESSAGES', 'READ_MESSAGES', 'EMBED_LINKS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'],
+    cooldown: 1500,
+    dev_only: true,
+    disabled: false,
+    execute(message, args) {
+        if (!fs.existsSync('guilddata.json')) fs.writeFileSync('guilddata.json', '{}');
+        let file = JSON.parse(fs.readFileSync('guilddata.json'));
+
+        let gid = message.guild.id;
+
+        // Fix data if format is incorrect
+        if (typeof file[gid] != 'object') file[gid] = {}
+            if (typeof file[gid].mutedRole != 'string') {
+            if (typeof file[gid].mutedRole == 'number' || typeof file[gid] == 'bigint')
+            file[gid].mutedRole = toString(file[gid].mutedRole);
+            else file[gid].mutedRole = undefined;
+        }
+        if (message.guild.roles.get(file[gid].mutedRole) == undefined) file[gid].mutedRole = undefined;
+
+        fs.writeFileSync('guilddata.json', JSON.stringify(file));
+        // ------------------------------
+
+
+        if (!message.mentions.members.first()) return message.channel.send('You need to mention the user you want to unmute.');
+        let target = message.mentions.members.first();
+
+        // Check if the target is muted
+        if (!target.roles.get(file[gid].mutedRole) || !file[gid].mutedRole) return message.channel.send(new Discord.RichEmbed()
+        .setTitle('Unable to unmute')
+        .setDescription(`${target.user.username} isn't muted.`)
+        .setTimestamp()
+        .setFooter(`Invoked by ${message.author.username}`, target.user.avatarURL)
+        );
+
+
+        // Unmute target
+        target.removeRole(message.guild.roles.get(file[gid].mutedRole).id).catch(e => {throw e});
+        message.channel.send(new Discord.RichEmbed()
+        .setTitle('User unmuted')
+        .setDescription(`Successfully unmuted ${target.user.username}#${target.user.discriminator}`)
+        .setTimestamp()
+        .setFooter(`Invoked by ${message.author.username}`, target.user.avatarURL)
+        );
+    }
+}
