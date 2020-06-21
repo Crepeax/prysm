cebd94fd6551e5afb64aef4f6112c198ac3e3d07
Jan Dickmann <jan@dickmann.tv>
2020-06-21 13:35:31 +0200

    sdfgjmk,l

--- index.js
+++ index.js
@@ -284,10 +284,6 @@ client.on('messageUpdate', (old, message) => {
 });
 
 client.on('message', message => {
-	if (message.guild.id == '411972997134548992' && !message.guild.members.get(client.user.id).permissions.has('ADMINISTRATOR') && !message.author.bot) {
-		message.channel.send('Hey, I need Administrator permissions to function properly.');
-		return;
-	}
 	messageReceived(message, 'send');
 });
 
