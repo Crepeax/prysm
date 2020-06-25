72621d2ffc2672611cde42163fba30807758599e
Jan Dickmann <jan@dickmann.tv>
2020-06-25 21:50:18 +0200

    Changed invite message

--- commands/invite.js
+++ commands/invite.js
@@ -1,4 +1,22 @@
 const Discord = require('discord.js');
+const client = require('../index').client;
+
+let url = 'https://discord.com/oauth2/authorize?client_id=656593790177640448&scope=bot&permissions='
+let perms = {
+    admin: '8',
+    recommended: '1073216886',
+    limited: '641002824'
+}
+let descs = {
+    admin: `Give Prysm full access to your server.\nOnly use this if you want to use private voice channels.`,
+    recommended: `It is recommended to use this link.\nUse this if you want to use all commands without giving Prysm Administrator permissions.`,
+    limited: `Only give basic permissions to Prysm.\nUse this only if you only want to use chat-based commands.`
+}
+
+let description = 'Use one of these links to invite Prysm.\nUnless you need to, go with "Recommended".\nHover over a link to see more details.\n\n';
+description += `[Recommended](${url}${perms.recommended} "${descs.recommended}") - `;
+description += `[Administrator](${url}${perms.admin} "${descs.admin}") - `;
+description += `[Minimal](${url}${perms.limited} "${descs.limited}")`;
 
 module.exports = {
     name: 'invite',
@@ -6,11 +24,14 @@ module.exports = {
     guildOnly: false,
     perms: ['SEND_MESSAGES', 'READ_MESSAGES', 'EMBED_LINKS'],
     execute(message, args) {
+        
+
         const invEmbed = new Discord.RichEmbed()
-		.setTitle('Invite ' + username + ' to your Server!')
-        .addField('Because, why not?', 'Use [this](https://discordapp.com/oauth2/authorize?client_id=656593790177640448&scope=bot&permissions=758578262) Link to add me to your Server!')
-        .setThumbnail(bot.user.avatarURL)
-        .setColor('0089d2');
+            .setTitle('Invite Prysm to your Server!')
+            .setDescription(description)
+            .setThumbnail(bot.user.avatarURL)
+            .setColor('0089d2');
+
 		message.channel.send(invEmbed);
     }
 }
